df5585a896214b27832ecef6e886aca44f051557
tiks1990 <heinz@fuimonos.co>
2015-01-05 12:16:38 -0500

    Initial Commit

---
+++ Kangu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kangu
+//
+//  Created by Heinz Sohm on 1/5/15.
+//  Copyright (c) 2015 Proximity2U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kangu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Kangu
+//
+//  Created by Heinz Sohm on 1/5/15.
+//  Copyright (c) 2015 Proximity2U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
